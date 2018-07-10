5b1b8b28b2957dce4de381e6f3b76ca05794cad8
eslamsherif <eng.islam.sherif@gmail.com>
2018-07-11 01:13:16 +0200

    Successful build and update the RMSE and Jacobian functions.

---
+++ build/CMakeFiles/ExtendedKF.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/src/FusionEKF.cpp" "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/build/CMakeFiles/ExtendedKF.dir/src/FusionEKF.cpp.o"
+  "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/src/kalman_filter.cpp" "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/build/CMakeFiles/ExtendedKF.dir/src/kalman_filter.cpp.o"
+  "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/src/main.cpp" "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/build/CMakeFiles/ExtendedKF.dir/src/main.cpp.o"
+  "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/src/tools.cpp" "/mnt/e/Backup/SelfDrivingNanoDegree/Term2/06_Extended_Kalman_Filter_Project/MySolution/build/CMakeFiles/ExtendedKF.dir/src/tools.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,58 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+   VectorXd rmse(4);
+   /* Initialize to some very large values */
+   rmse << 1000,1000,1000,1000;
+
+   /* check the validity of the inputs, Sanity Checks */
+   if ( ( estimations.size() == 0 ) || ( estimations.size() != ground_truth.size() ) )
+   {
+      return rmse;
+   }
+
+   /* Accumulate squared residuals */
+   for(unsigned int idx=0; idx < estimations.size(); ++idx){
+      VectorXd temp = estimations[idx] - ground_truth[idx];
+      temp = temp.array() * temp.array();
+      rmse += temp;
+   }
+
+   /* Calculate the mean */
+   rmse /= estimations.size();
+
+   /* Calculate the squared root */
+   rmse = rmse.array().sqrt();
+
+   return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+
+   MatrixXd Hj(3,4);
+   /* Recover state parameters  */
+
+   float px = x_state(0);
+   float py = x_state(1);
+   float vx = x_state(2);
+   float vy = x_state(3);
+
+   /* Check division by zero */
+   if ( (px == 0) || (py == 0) )
+   {
+      Hj << 0,0,0,0,
+            0,0,0,0,
+            0,0,0,0;
+   }
+   else
+   {
+      float polar_dist = (px*px) + (py*py);
+      float sqrt_polar_dist = sqrt(polar_dist);
+
+      Hj << ((px)/(sqrt_polar_dist)),((py)/(sqrt_polar_dist)),(0),(0),
+            ((-1*py)/(polar_dist))  ,((px)/(polar_dist))     ,(0),(0),
+            ((py*(vx*py-vy*px))/(sqrt_polar_dist*polar_dist)),((px*(vy*px-vx*py))/(sqrt_polar_dist*polar_dist)),((px)/(sqrt_polar_dist)),((py)/(sqrt_polar_dist));
+   }
+
+   return Hj;
 }
